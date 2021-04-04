082f7ff5a18b9857f98533ed4c1bac578ba0a06e
maloslov <60790985+maloslov@users.noreply.github.com>
2021-04-04 16:28:55 +0300

    basic work

---
+++ Ping/ICMP.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Ping
+{
+    class ICMP
+    {
+        public byte Type;
+        public byte Code;
+        public UInt16 Checksum;
+        public int MessageSize;
+        public byte[] Message;
+
+        public ICMP()
+        {
+            Type = 8;
+            Code = 0;
+            Message = new byte[32];
+            for (int i = 0; i < Message.Length; i++)
+                Message[i] = 1;
+        }
+
+        public ICMP(byte[] data, int size)
+        {
+            Type = data[20];
+            Code = data[21];
+            Checksum = BitConverter.ToUInt16(data, 22);
+            MessageSize = size - 24;
+            Message = new byte[MessageSize];
+            Buffer.BlockCopy(data, 24, Message, 0, MessageSize);
+        }
+
+        public byte[] getBytes()
+        {
+            byte[] data = new byte[MessageSize + 9];
+            Buffer.BlockCopy(BitConverter.GetBytes(Type), 0, data, 0, 1);
+            Buffer.BlockCopy(BitConverter.GetBytes(Code), 0, data, 1, 1);
+            Buffer.BlockCopy(BitConverter.GetBytes(Checksum), 0, data, 2, 2);
+            Buffer.BlockCopy(Message, 0, data, 4, MessageSize);
+            return data;
+        }
+
+        public UInt16 getChecksum()
+        {
+            UInt32 chcksm = 0;
+            byte[] data = getBytes();
+            int packetsize = MessageSize + 8;
+            int index = 0;
+
+            while (index < packetsize)
+            {
+                chcksm += Convert.ToUInt32(BitConverter.ToUInt16(data, index));
+                index += 2;
+            }
+            chcksm = (chcksm >> 16) + (chcksm & 0xffff);
+            chcksm += (chcksm >> 16);
+            return (UInt16)(~chcksm);
+        }
+    }
+}
--- Ping/Program.cs
+++ Ping/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Ping
 {
@@ -9,6 +8,26 @@ namespace Ping
     {
         static void Main(string[] args)
         {
+            var host = IPAddress.Any;
+            Console.WriteLine("Input ip:");
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
+            var remoteEP = new IPEndPoint(IPAddress.Parse(Console.ReadLine()), 0);
+
+            var pack = new ICMP();
+            pack.Checksum = pack.getChecksum();
+            Console.WriteLine(BitConverter.ToString(pack.getBytes()));
+            socket.Bind(new IPEndPoint(host, 0));
+            var timer = System.Diagnostics.Stopwatch.StartNew();
+            socket.SendTo(pack.getBytes(), remoteEP);
+            byte[] buffer = new byte[32];
+            socket.Receive(buffer);
+            timer.Stop();
+            var pack2 = new ICMP(buffer, buffer.Length);
+            var ipfrom = new byte[4];
+            Buffer.BlockCopy(buffer, 12, ipfrom, 0, 4);
+            Console.WriteLine("Received from " + new IPAddress(ipfrom).ToString());
+            Console.WriteLine("Type: " + pack2.Type + " Code: " + pack2.Code);
+            Console.WriteLine("Time: " + timer.ElapsedMilliseconds + " ms");
         }
     }
 }
